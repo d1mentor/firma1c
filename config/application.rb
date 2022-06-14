4b00b1ccc3d620c0e9b8c8dd284a16a46bb6b510
d1mentor <gerasimenkot92@gmail.com>
2022-06-14 20:44:05 +0300

    Локализация, стили, финалочка фронта

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,14 @@ module Firma1c
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # где библиотека I18n должна искать наши переводы
+I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
+
+# Разрешенные локали, доступные приложению
+I18n.available_locales = [:en, :ru]
+
+# устанавливаем локаль по умолчанию на что-либо другое, чем :en
+I18n.default_locale = :ru
   end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -308,7 +308,7 @@ end
   # config.navigational_formats = ['*/*', :html]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
-  config.sign_out_via = :delete
+  config.sign_out_via = :get
 
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting

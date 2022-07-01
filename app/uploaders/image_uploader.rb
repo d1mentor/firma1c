2a0904707fdadc2fb9c8be8c2c48dad3404a4d13
d1mentor <gerasimenkot92@gmail.com>
2022-07-01 21:02:37 +0300

    Инструменты таблицами, превью в таблице инструменты, превью файлов в галереях

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,6 +13,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.name}"
   end
 
+    def image?(carrier_wave_sanitized_file)
+      true
+    end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:

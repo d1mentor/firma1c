1625895de8f549467fc674937bc10970718cb14d
d1mentor <gerasimenkot92@gmail.com>
2022-07-18 23:48:08 +0300

    Сессия теперь умирает после 15 минут бездействия

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :timeoutable, :validatable
   audited
 
   def logs

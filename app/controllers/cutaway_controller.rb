aeb49c59d065997f6cdd5e630526e678234fded7
d1mentor <gerasimenkot92@gmail.com>
2022-08-04 18:20:22 +0300

    cutaway backend

---
+++ app/controllers/cutaway_controller.rb
@@ -0,0 +1,16 @@
+class CutawayController < ApplicationController
+  def index
+  end
+
+  def about_us
+  end
+
+  def contacts
+  end
+  
+  def portfolio
+  end
+
+  def services    
+  end  
+end

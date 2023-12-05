e162131a8cb5540edc4f5762d02ec767392b3bbe
d1mentor <gerasimenkot92@gmail.com>
2023-12-05 17:28:57 +0200

    lock cap version 3.18.0

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :keep_releases, 5
 
 
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.17.0"
+lock "~> 3.18.0"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
